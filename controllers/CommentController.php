de504a27b5320d926ad09076f6604f2d1dcc9833
최인아 <jody980502@gmail.com>
2020-09-06 20:30:59 +0900

    getCommentReply

--- controllers/CommentController.php
+++ controllers/CommentController.php
@@ -213,7 +213,15 @@ try {
             if (!is_integer($postIdx)) {
                 $res->isSuccess = FALSE;
                 $res->code = 412;
-                $res->message = "게시물 idx 타입이 잘못되었습니다";
+                $res->message = "게시물 idx는 Int 이여야 합니다";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                addErrorLogs($errorLogs, $res, $req);
+                return;
+            }
+            if($page < 1){
+                $res->isSuccess = FALSE;
+                $res->code = 430;
+                $res->message = "page는 1부터 시작입니다";
                 echo json_encode($res, JSON_NUMERIC_CHECK);
                 addErrorLogs($errorLogs, $res, $req);
                 return;
@@ -235,12 +243,118 @@ try {
                 return;
             }
 
-            $res->page = $page;
-            $res->limit = $limit;
             $res->result = getComment($userIdx, $postIdx, $page, $limit);
             $res->isSuccess = TRUE;
             $res->code = 200;
             $res->message = "댓글 조회 완료";
+
+            echo json_encode($res, JSON_NUMERIC_CHECK);
+            break;
+
+        case "getCommentReply":
+            http_response_code(200);
+
+            $jwt = $_SERVER["HTTP_X_ACCESS_TOKEN"];
+
+            if (!isValidHeader($jwt, JWT_SECRET_KEY)) {
+                $res->isSuccess = FALSE;
+                $res->code = 450;
+                $res->message = "존재하지 않는 유저입니다";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                addErrorLogs($errorLogs, $res, $req);
+                return;
+            }
+
+            $data = getDataByJWToken($jwt, JWT_SECRET_KEY);
+            $userIdx = getUserIdxFromId($data->id);
+
+            $commentIdx = $vars["idx"];
+            $commentIdx = isset($commentIdx) ? intval($commentIdx) : null;
+
+            $page = $_GET["page"];
+            $page = isset($page) ? intval($page) : null;
+            $limit = $_GET["limit"];
+            $limit = isset($limit) ? intval($limit) : null;
+
+            if (is_null($page)) {
+                $res->isSuccess = FALSE;
+                $res->code = 440;
+                $res->message = "page가 null 입니다";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                addErrorLogs($errorLogs, $res, $req);
+                return;
+            }
+            if (is_null($limit)) {
+                $res->isSuccess = FALSE;
+                $res->code = 441;
+                $res->message = "limit가 null 입니다";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                addErrorLogs($errorLogs, $res, $req);
+                return;
+            }
+            if ($postIdx == null) {
+                $res->isSuccess = FALSE;
+                $res->code = 442;
+                $res->message = "댓글 idx가 null 입니다";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                addErrorLogs($errorLogs, $res, $req);
+                return;
+            }
+
+            if (gettype($page) != 'integer') {
+                $res->isSuccess = FALSE;
+                $res->code = 410;
+                $res->message = "page는 Int 이여야 합니다";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                addErrorLogs($errorLogs, $res, $req);
+                return;
+            }
+
+            if (gettype($limit) != 'integer') {
+                $res->isSuccess = FALSE;
+                $res->code = 411;
+                $res->message = "limit는 Int 이여야 합니다";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                addErrorLogs($errorLogs, $res, $req);
+                return;
+            }
+            if (!is_integer($postIdx)) {
+                $res->isSuccess = FALSE;
+                $res->code = 412;
+                $res->message = "댓글 idx는 Int 이여야 합니다";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                addErrorLogs($errorLogs, $res, $req);
+                return;
+            }
+            if($page < 1){
+                $res->isSuccess = FALSE;
+                $res->code = 430;
+                $res->message = "page는 1부터 시작입니다";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                addErrorLogs($errorLogs, $res, $req);
+                return;
+            }
+            if(!isCommentExist($commentIdx)) {
+                $res->isSuccess = FALSE;
+                $res->code = 451;
+                $res->message = "존재하지 않는 댓글 idx 입니다";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                addErrorLogs($errorLogs, $res, $req);
+                return;
+            }
+            if(!isCommentReplyExist($commentIdx)) {
+                $res->isSuccess = FALSE;
+                $res->code = 452;
+                $res->message = "조회 할 답글이 없습니다";
+                echo json_encode($res, JSON_NUMERIC_CHECK);
+                addErrorLogs($errorLogs, $res, $req);
+                return;
+            }
+
+            $res->result = getCommentReply($userIdx, $postIdx, $page, $limit);
+            $res->isSuccess = TRUE;
+            $res->code = 200;
+            $res->message = "답글 조회 완료";
 
             echo json_encode($res, JSON_NUMERIC_CHECK);
             break;
