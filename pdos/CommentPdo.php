e609a6794428a6f470c55dd2b4d7df892313805b
최인아 <jody980502@gmail.com>
2020-09-09 01:16:14 +0900

    send_notification

--- pdos/CommentPdo.php
+++ pdos/CommentPdo.php
@@ -486,3 +486,38 @@ function getNameFromIdx($userIdx)
 
     return intval($res[0]["userName"]);
 }
+
+
+function send_notification($tokens, $message)
+{
+    $url = 'https://fcm.googleapis.com/fcm/send';
+    $fields = array(
+        'registration_ids' => $tokens,
+        'data'             => $message
+    );
+
+    $headers = array(
+        'Authorization:key =' . self::GOOGLE_FCM_API_KEY,
+        'Content-Type: application/json'
+    );
+    /*
+    print_r($fields);
+    print_r($headers);
+    exit;
+    */
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+    $result = curl_exec($ch);
+    if ($result === FALSE) {
+        die('Curl failed: ' . curl_error($ch));
+    }
+    curl_close($ch);
+    return $result;
+}
