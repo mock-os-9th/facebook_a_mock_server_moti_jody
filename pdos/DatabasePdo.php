a6b7141e60c4f8f0e8bf57f365636a4a9d2c0c7b
최인아 <jody980502@gmail.com>
2020-09-08 22:45:23 +0900

    ApiKey

--- pdos/DatabasePdo.php
+++ pdos/DatabasePdo.php
@@ -8,6 +8,8 @@ function pdoSqlConnect()
         $DB_NAME = "facebookDev";
         $DB_USER = "facebookDev";
         $DB_PW = "lyunj2020!";
+
+        $GOOGLE_API_KEY = "AAAAuTKmVM0:APA91bHwf4e40fq1oq9nYUoMAGE12AlpZ58WViaQdsEqYqTqHVdV7zimDMTJvp7GjkdhSXI1qp8gH_qhMl8ooyOjsJqf4SDOHbV3avyguHijNat-aG_wsxQKyJP_NBKWcKkYDhgtN4Ob";
         $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PW);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
