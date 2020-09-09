474adbdf7a05444cca46092c5c114a418858746b
LyunJ <59385706+LyunJ@users.noreply.github.com>
2020-09-09 13:32:51 +0900

    bug fix

--- pdos/FcmFriendRecommendPdo.php
+++ pdos/FcmFriendRecommendPdo.php
@@ -1,21 +1,5 @@
 <?php
-function pdoSqlConnect()
-{
-    try {
-        $DB_HOST = "facebookdev.cevu32mso1il.ap-northeast-2.rds.amazonaws.com";
-        $DB_NAME = "facebookDev";
-        $DB_USER = "facebookDev";
-        $DB_PW = "lyunj2020!";
-
-        $GOOGLE_API_KEY = "AAAAuTKmVM0:APA91bHwf4e40fq1oq9nYUoMAGE12AlpZ58WViaQdsEqYqTqHVdV7zimDMTJvp7GjkdhSXI1qp8gH_qhMl8ooyOjsJqf4SDOHbV3avyguHijNat-aG_wsxQKyJP_NBKWcKkYDhgtN4Ob";
-
-        $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PW);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch (\Exception $e) {
-        echo $e->getMessage();
-    }
-}
+require './DatabasePdo.php';
 
 $pdo = pdoSqlConnect();
 $query = "select UserFriends.userIdx,UserFriends.friendIdx, FriendFriend.friendIdx as recommendUserIdx, count(FriendFriend.friendIdx) as count
