7bbd61d826dd8dd2d6e9070579806f6574d43e63
최인아 <jody980502@gmail.com>
2020-09-09 14:32:55 +0900

    check userIdx

--- controllers/CommentController.php
+++ controllers/CommentController.php
@@ -492,7 +492,7 @@ try {
             }
 
             //$res->commentIdx = createComment($userIdx, $postIdx, $commentContent, $commentImgUrl);
-            echo getNameFromIdx($userIdx);
+            echo $userIdx;
             send_comment_noti($userIdx, $postIdx, $commentContent);
             $res->isSuccess = TRUE;
             $res->code = 200;
