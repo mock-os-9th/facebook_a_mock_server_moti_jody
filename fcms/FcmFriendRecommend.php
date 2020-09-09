837991b4628bee4000c79231171710ea4f48f2fc
LyunJ <59385706+LyunJ@users.noreply.github.com>
2020-09-09 19:03:21 +0900

    모두 추천했으면 초기화하여 다시 처음부터 추천기능 추가

--- fcms/FcmFriendRecommend.php
+++ fcms/FcmFriendRecommend.php
@@ -4,6 +4,22 @@ require '/home/ubuntu/api-server/pdos/FriendPdo.php';
 
 $pdo = pdoSqlConnect();
 
+$query = "select userIdx,bit_and(isDeleted='Y') as isNeedReset from FriendRecommend group by userIdx having isNeedReset = 1;";
+$st = $pdo->prepare($query);
+$st->execute();
+
+$st->setFetchMode(PDO::FETCH_ASSOC);
+$reset = $st->fetchAll();
+
+if(!is_null($reset)){
+    foreach ($reset as $key => $item){
+        $query = "update FriendRecommend set isDeleted = 'N' where userIdx = ?";
+        $st = $pdo->prepare($query);
+        $st->execute([$item['userIdx']]);
+    }
+}
+
+
 $query = "select UserToken.token,
        UserToken.userIdx,
        json_arrayagg(json_object('recommendUserIdx', FriendRecommend.recommendUserIdx, 'recommendUserName',
@@ -22,35 +38,34 @@ $res = $st->fetchAll();
 
 
 foreach ($res as $key => $item){
-    $item['recommendUserIdxName'] = json_decode($item['recommendUserIdxName'],true);
-    echo $item['recommendUserIdxName'][0]['recommendUserIdx'];
-//    $message = '새로운 친구 추천이 있습니다 : '.$item['recommendUserIdxName']['recommendUserName'].'님';
-//    $url='https://fcm.googleapis.com/fcm/send';
-//    $notification = array('body'=>$message,'title'=>'친구추천알림');
-//    $to = $item['token'];
-//    $fields = array('notification'=>$notification,'to'=>$to);
-//
-//    $key = "AAAAuTKmVM0:APA91bHwf4e40fq1oq9nYUoMAGE12AlpZ58WViaQdsEqYqTqHVdV7zimDMTJvp7GjkdhSXI1qp8gH_qhMl8ooyOjsJqf4SDOHbV3avyguHijNat-aG_wsxQKyJP_NBKWcKkYDhgtN4Ob";
-//    $headers = array('Authorization:key = '.$key,'Content-Type:application/json');
-//
-//    $ch = curl_init();
-//    curl_setopt($ch, CURLOPT_URL, $url);
-//    curl_setopt($ch, CURLOPT_POST, true);
-//    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//    curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
-//    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-//    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-//
-//    $result = curl_exec($ch);
-//    if($result === false){
-//        die('Curl failed:'.curl_error($ch));
-//    }
-//    curl_close($ch);
-//
-//    addFriendNotification(intval($item['userIdx']),intval($item['recommendUserIdxName']['recommendUserIdx']),$message);
-//
-//    $query = "update FriendRecommend set isDeleted = 'Y' where userIdx = ? and recommendUserIdx = ?";
-//    $st = $pdo->prepare($query);
-//    $st->execute([intval($item['userIdx']),intval($item['recommendUserIdxName']['recommendUserIdx'])]);
+    $recommendUser = json_decode($item['recommendUserIdxName'],true);
+    $message = '새로운 친구 추천이 있습니다 : '.$recommendUser[0]['recommendUserName'].'님';
+    $url='https://fcm.googleapis.com/fcm/send';
+    $notification = array('body'=>$message,'title'=>'친구추천알림');
+    $to = $item['token'];
+    $fields = array('notification'=>$notification,'to'=>$to);
+
+    $key = "AAAAuTKmVM0:APA91bHwf4e40fq1oq9nYUoMAGE12AlpZ58WViaQdsEqYqTqHVdV7zimDMTJvp7GjkdhSXI1qp8gH_qhMl8ooyOjsJqf4SDOHbV3avyguHijNat-aG_wsxQKyJP_NBKWcKkYDhgtN4Ob";
+    $headers = array('Authorization:key = '.$key,'Content-Type:application/json');
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+
+    $result = curl_exec($ch);
+    if($result === false){
+        die('Curl failed:'.curl_error($ch));
+    }
+    curl_close($ch);
+
+    addFriendNotification(intval($item['userIdx']),intval($recommendUser[0]['recommendUserIdx']),$message);
+
+    $query = "update FriendRecommend set isDeleted = 'Y' where userIdx = ? and recommendUserIdx = ?";
+    $st = $pdo->prepare($query);
+    $st->execute([intval($item['userIdx']),intval($recommendUser[0]['recommendUserIdx'])]);
 }
