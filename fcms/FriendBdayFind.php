2c8131f5278082afe3d509467242a0807ee84da5
최인아 <jody980502@gmail.com>
2020-09-09 21:58:03 +0900

    getUserIdxByToken added

--- fcms/FriendBdayFind.php
+++ fcms/FriendBdayFind.php
@@ -55,6 +55,38 @@ $link = "http://54.180.68.232/user/50/profile/info";
         }
     }
 
+function send_friend_bday_notification($token, $message)
+{
+    $GOOGLE_API_KEY = "AAAAuTKmVM0:APA91bHwf4e40fq1oq9nYUoMAGE12AlpZ58WViaQdsEqYqTqHVdV7zimDMTJvp7GjkdhSXI1qp8gH_qhMl8ooyOjsJqf4SDOHbV3avyguHijNat-aG_wsxQKyJP_NBKWcKkYDhgtN4Ob";
+    $url = 'https://fcm.googleapis.com/fcm/send';
+
+    $fields = array(
+        'to' => $token,
+        'notification'             => $message
+    );
+
+    $headers = array(
+        'Authorization:key =' . $GOOGLE_API_KEY,
+        'Content-Type: application/json'
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+    $result = curl_exec($ch);
+    if ($result === FALSE) {
+        die('Curl failed: ' . curl_error($ch));
+    }
+    curl_close($ch);
+    return $result;
+}
+
+
 function getUserIdxByToken($token)
 {
     $pdo = pdoSqlConnect();
