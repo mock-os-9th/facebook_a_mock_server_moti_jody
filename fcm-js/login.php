db9349cbda58c61cda6e5618b7bd2621032ea944
LyunJ <59385706+LyunJ@users.noreply.github.com>
2020-09-10 01:50:02 +0900

    jwt echo test

--- fcm-js/login.php
+++ fcm-js/login.php
@@ -3,30 +3,28 @@ $id = $_POST['id'];
 $pw = $_POST['pw'];
 $token = $_POST['token'];
 
-echo $token;
-echo ("<script language=javascript> console.log($token);</script>");
-//$url='http://15.164.195.62/login';
-//$fields = array('id',$id,'pw',$pw);
-//
-//$headers = array('Content-Type:application/json');
-//
-//$ch = curl_init();
-//curl_setopt($ch, CURLOPT_URL, $url);
-//curl_setopt($ch, CURLOPT_POST, true);
-//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
-//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-//curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-//
-//$result = curl_exec($ch);
-//if($result === false){
-//    die('Curl failed:'.curl_error($ch));
-//}
-//curl_close($ch);
-//
-//$jwt = json_decode($result);
-//$jwt = $jwt->result->jwt;
-//echo $jwt;
+$url='http://15.164.195.62/login';
+$fields = array('id',$id,'pw',$pw);
+
+$headers = array('Content-Type:application/json');
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+
+$result = curl_exec($ch);
+if($result === false){
+    die('Curl failed:'.curl_error($ch));
+}
+curl_close($ch);
+
+$jwt = json_decode($result);
+$jwt = $jwt->result->jwt;
+echo $jwt;
 
 
