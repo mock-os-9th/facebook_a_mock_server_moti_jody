9cab3bbd0e3078d5011ca252e244c3f181a9a896
LyunJ <59385706+LyunJ@users.noreply.github.com>
2020-09-09 18:38:14 +0900

    test

--- fcms/FcmFriendRecommend.php
+++ fcms/FcmFriendRecommend.php
@@ -23,7 +23,7 @@ $res = $st->fetchAll();
 
 foreach ($res as $key => $item){
     $item['recommendUserIdxName'] = json_decode($item['recommendUserIdxName']);
-    echo $item['recommendUserIdxName']['recommendUserIdx'],$item['recommendUserIdxName']['recommendUserName'];
+    echo $item['recommendUserIdxName']->recommendUserIdx,$item['recommendUserIdxName']->recommandUserName;
 //    $message = '새로운 친구 추천이 있습니다 : '.$item['recommendUserIdxName']['recommendUserName'].'님';
 //    $url='https://fcm.googleapis.com/fcm/send';
 //    $notification = array('body'=>$message,'title'=>'친구추천알림');
